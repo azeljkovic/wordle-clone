aa65df503062d26bd6cf6f9417d23aa8188632aa
Aleksandar Zeljkovic <aleksandar.zeljkovic91@gmail.com>
2022-09-09 15:57:42 +0200

    added letter fillout

--- js/main.js
+++ js/main.js
@@ -1,4 +1,21 @@
-let x = 5;
-x += 10;
+let grid = document.querySelector('.container');
+let letterBoxSelector = '';
+let letterIndex = 0;
 
-console.log(x);
+
+document.addEventListener('keydown', printLetter);
+
+
+function printLetter(e) {
+  if (isLetter(e.key)) {
+    letterBoxSelector = `.letter-${letterIndex}`;
+    let letterBoxElement = document.querySelector(letterBoxSelector);
+    letterBoxElement.textContent = e.key.toUpperCase();
+    letterIndex++;
+  }
+}
+
+
+function isLetter(letter) {
+  return /^[a-zA-Z]$/.test(letter);
+}
