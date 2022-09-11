075456639060d1c8dfa32646d0449177732ef41e
Aleksandar Zeljkovic <aleksandar.zeljkovic91@gmail.com>
2022-09-11 22:03:03 +0200

    moved comments

--- js/main.js
+++ js/main.js
@@ -8,17 +8,19 @@ let letterBoxElement = '';
 let letterIndex = 0;
 let wordEndIndex = 5;
 let wordStartIndex = 0;
-let currentWord = '';
+let currentWord;
 
 
 document.addEventListener('keydown', printLetter);
 
 function printLetter(e) {
-  if (isLetter(e.key) && letterIndex < wordEndIndex) { // letter key handler
+  // letter key handler
+  if (isLetter(e.key) && letterIndex < wordEndIndex) {
     letterBoxElement = getLetterBoxElement(letterIndex);
     letterBoxElement.textContent = e.key.toUpperCase();
     letterIndex++;
-  } else if (e.key === 'Backspace') { // backspace handler
+    // backspace handler
+  } else if (e.key === 'Backspace') {
     if (letterIndex !== wordStartIndex) {
       if (letterIndex !== 0) {
         letterIndex--;
@@ -26,10 +28,12 @@ function printLetter(e) {
       letterBoxElement = getLetterBoxElement(letterIndex);
       letterBoxElement.textContent = '';
     }
-  } else if (e.key === 'Enter') { // enter handler
+    // enter handler
+  } else if (e.key === 'Enter') {
     currentWord = '';
     if (letterIndex === wordEndIndex) {
-      for (let i = wordEndIndex - 5; i < letterIndex; i++) { // assemble current word
+      // assemble current word
+      for (let i = wordEndIndex - 5; i < letterIndex; i++) {
         letterBoxElement = getLetterBoxElement(i);
         currentWord += letterBoxElement.textContent;
         currentWord = currentWord.toLowerCase();
@@ -37,8 +41,9 @@ function printLetter(e) {
       if (!checkIfWordExistInDictionary(currentWord)) {
         launchToast();
       } else {
-        if(checkWord(currentWord, wordStartIndex)){
-          letterIndex = 30; // disable further input by moving letter index to the end
+        if (checkWord(currentWord, wordStartIndex)) {
+          // disable further input by moving letter index to the end
+          letterIndex = 30;
         } else {
           wordStartIndex += 5;
           wordEndIndex += 5;
